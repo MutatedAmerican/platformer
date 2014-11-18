b96fbfd8132f1ed5925f5956ca60ca2c843715e2
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-11-18 15:07:27 -0800

    Rendered and Load Level Map

--- core/src/com/diazb/platformer/Platformer.java
+++ core/src/com/diazb/platformer/Platformer.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.Game;
 import com.diazb.platformer.view.GameScreen;
 
 public class Platformer extends Game {
-    
     @Override
     public void create() {
         //set screen and display level
