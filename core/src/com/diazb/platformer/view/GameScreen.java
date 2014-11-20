741dab171edfba08d451a691cc142fc1c69a9652
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-11-20 14:48:24 -0800

    Thursday, November 20, 2014 Milestone 10-Fixing the Aspect Ratio

--- core/src/com/diazb/platformer/view/GameScreen.java
+++ core/src/com/diazb/platformer/view/GameScreen.java
@@ -1,6 +1,7 @@
 package com.diazb.platformer.view;
 //package and import classes
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -18,10 +19,15 @@ public class GameScreen implements Screen{
         map = new TmxMapLoader().load("map/level0-1.tmx");
         //render up tiles on the map
         renderer= new OrthogonalTiledMapRenderer(map, 1/70f);
+        //get the height and width of the screen and store them into variables
+        float width= Gdx.graphics.getWidth();
+        float height= Gdx.graphics.getHeight();
         //create camera's length and height variables
-        camera= new OrthographicCamera(14f, 14f);
+        //modify code to change aspect ratio
+        camera= new OrthographicCamera(14f, 14f * (height/width));
         //set the position of the camera on the level
-        camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2, 0f);
+
+        camera.position.set(camera.viewportWidth/2f, camera.viewportHeight/2f, 0f);
     }
 
     @Override
