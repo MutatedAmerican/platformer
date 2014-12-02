40d3281b2cc42760e028a6f53f3753b09c78090b
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-12-02 14:38:22 -0800

    Creation of the Player Class

---
+++ core/src/com/diazb/platformer/model/Player.java
@@ -0,0 +1,20 @@
+package com.diazb.platformer.model;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+public class Player {
+    //create x and y position on screen
+    public Vector2 position;
+    //store image on the screen
+    public Texture spriteSheet;
+
+    //generate constructor for player
+    public Player() {
+        //origin at the screen [(0,0) is bottom left]
+        position= new Vector2(0, 0);
+        //path to get texture to store the player images
+        spriteSheet= new Texture(Gdx.files.internal("img/aliens.png"));
+    }
+}
