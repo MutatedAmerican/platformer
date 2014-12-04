32467b28fbf37e23963213bde5f08eb0c4063ab4
Bryan Diaz <bryandiaz2010@yahoo.com>
2014-12-04 15:03:34 -0800

    Take it Sprite Sheet, Cut Up All Aliens, Number Them, and Display Selected Alien

--- core/src/com/diazb/platformer/model/Player.java
+++ core/src/com/diazb/platformer/model/Player.java
@@ -3,6 +3,7 @@ package com.diazb.platformer.model;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 public class Player {
@@ -10,6 +11,8 @@ public class Player {
     public Vector2 position;
     //store image on the screen
     public Texture spriteSheet;
+    //cut up the sprite sheet
+    public TextureRegion[] spriteFrames;
 
     //generate constructor for player
     public Player() {
@@ -17,10 +20,35 @@ public class Player {
         position= new Vector2(0, 0);
         //path to get texture to store the player images
         spriteSheet= new Texture(Gdx.files.internal("img/aliens.png"));
+        //splits the sprite sheets into width and height
+        TextureRegion[][] spriteSheetFrames= TextureRegion.split(spriteSheet, 70, 100);
+        //start at origin
+        int counter=0;
+        //at the origin, go through the sprite sheet rows
+        for(int row= 0; row < spriteSheetFrames.length; row++){
+            //at the origin, go through the sprite sheet column
+            for(int column= 0; column < spriteSheetFrames[row].length; column++){
+                //count how many aliens are in the sprite sheet
+                counter++;
+            }
+        }
+        //creates space for all aliens to store in memory
+        spriteFrames= new TextureRegion[counter];
+        //reset counter
+        counter=0;
+        //take in spriteSheetFrames and store it in a variable; like wild loop
+        for(TextureRegion[] row: spriteSheetFrames){
+            //take out each column from row and store it in a variable
+            for(TextureRegion sprite: row){
+                //access aliens on sprite sheet
+                spriteFrames[counter++]= sprite;
+            }
+        }
+
     }
     //draw character
     public void draw(Batch spriteBatch){
-        spriteBatch.draw(spriteSheet, 0, 0, 70, 100);
+        spriteBatch.draw(spriteFrames[22], 0, 0, 70, 100);
     }
     //update properties on the character constantly
     public void update(float deltaTime){
